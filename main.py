2e4df85444bf4cbae0d673fa5cbb79392d3e646a
Jao-aqui <joao_pedro_aquino@usp.br>
2026-05-22 23:26:59 -0300

    Adicionadas condicionais para criação de camadas

--- main.py
+++ main.py
@@ -10,22 +10,54 @@ from interpretadores import carregar_entrada_txt, carregar_saidas_one_hot
 
 rede = RedeNeural()
 
-# entrada: 120, as imagens tem 10 de altura e 12 de largura
-# oculta: 60
-rede.adicionar_camada(
-    Camada(
-        num_neuronios=60,
-        num_entradas=120
-    )
-)
+tamanho_do_dado=input("Selecione as dimensões do dado \n 1: 12x10 (para identificação de caractéres) \n 2: 2x1 (para algebra booleana)")
 
-# saída: 26 letras, 26 neurônios para possibilita em one hot
-rede.adicionar_camada(
-    Camada(
-        num_neuronios=26,
-        num_entradas=60
-    )
-)
+match tamanho_do_dado:
+    case 1:
+        numero_de_camadas=input("Selecione o número de camadas ocultas usados para identificar caractéres")
+        #a primeira camada tem que ter exatamente 120 entradas, mas as seguintes podem ter mais ou menos baseado no número de neurônios na camada anterior
+    case 2:
+        tipo_de_bool=input("Selecione o tipo de problema a resolver \n 1:AND ou OR \n 2:OR \n XOR")
+        
+
+
+match tamanho_do_dado:
+    case 1:
+       #Camada de entrada deve ter 120 entradas
+       rede.adicionar_camada(Camada(
+           num_neuronios=1,
+           num_entradas=120
+       ))
+       
+        # saída: 26 letras, 26 neurônios para possibilitar one hot
+        rede.adicionar_camada(Camada(
+            num_neuronios=26,
+            num_entradas=60
+            )
+        )
+        
+        
+    
+    
+    case 2:
+        if(tipo_de_bool==1):
+            rede.adicionar_camada(Camada(
+                num_neuronios=1,
+                num_entradas=2
+            ))
+        if(tipo_de_bool==2):
+            rede.adicionar_camada(Camada(
+                num_neuronios=2,
+                num_entradas=2
+                ))
+            rede.adicionar_camada(Camada(
+                num_neuronios=1,
+                num_entradas=2
+            ))
+        
+
+numero_de_epocas=input("Selecione o número de épocas ")
+
 
 
 # ======================================
@@ -34,10 +66,12 @@ rede.adicionar_camada(
 
 dados = []
 saidas = []
-entrada_A=carregar_entrada_txt(CARACTERES COMPLETO/X.txt)
+
+
+entrada_A=carregar_entrada_txt("CARACTERES COMPLETO/X.txt")
 dados.append(entrada_A)
 
-saida_A=carregar_saidas_one_hot(CARACTERES COMPLETO/Y_letra.txt)
+saida_A=carregar_saidas_one_hot("CARACTERES COMPLETO/Y_letra.txt")
 saidas.append(saida_A)
 # exemplo:
 # entrada_A = carregar_entrada_txt("CARACTERES COMPLETO/X.txt")
@@ -55,7 +89,7 @@ if len(dados) > 0:
     rede.treinar(
         dados,
         saidas,
-        epochs=1000,
+        epocas=numero_de_epocas,
         taxa_aprendizado=0.1
     )
 
--- rede/func_ativ.py
+++ rede/func_ativ.py
@@ -8,3 +8,26 @@ def sigmoid(x):
 def derivada_sigmoid(x):
     s = sigmoid(x)
     return s * (1 - s)
+
+def relu(x):
+    return max(0, x)
+
+
+def derivada_relu(x):
+    if x > 0:
+        return 1
+    return 0
+
+
+def tanh(x):
+    return math.tanh(x)
+
+
+def derivada_tanh(x):
+    t = tanh(x)
+    return 1 - t**2
+
+def step(x):
+    if x>0: return 1
+    if x==0: return 0
+    if x<0: return 0
